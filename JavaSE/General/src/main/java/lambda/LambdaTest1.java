032d8ba43394a9fcf91e03e0057005f4450be402
SkyFervor <skyfervor@gmail.com>
2017-12-20 13:42:56 +0800

    Update JaveSE General

--- JavaSE/General/src/main/java/lambda/LambdaTest1.java
+++ JavaSE/General/src/main/java/lambda/LambdaTest1.java
@@ -1,10 +1,24 @@
 package lambda;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * Created by shuhaoz
  * 2017/07/04 18:05
  */
 public class LambdaTest1 {
 	public static void main(String[] args) {
+		testJoining();
+	}
+
+	private static void testJoining() {
+		Entity e1 = new Entity(1, 1, 1);
+		Entity e2 = new Entity(2, 2, 2);
+		Object[] keys = new Object[]{e1, e2};
+		String result = Arrays.stream(keys)
+				.map(Object::toString)
+				.collect(Collectors.joining("-"));
+		System.out.println(result);
 	}
 }
